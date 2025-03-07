e3ad3ec4b3b55563816f39c49f1b7d0f511fed67
Renato Longobardi <renato.longobardi@gmail.com>
2025-03-07 12:25:57 +0100

    added leva ui

--- src/CubeExample.tsx
+++ src/CubeExample.tsx
@@ -1,13 +1,18 @@
 import * as THREE from 'three';
 import { useRef } from 'react';
 import { useFrame } from '@react-three/fiber';
+import { useControls } from 'leva';
 
 const CubeExample = () => {
 	const meshRef = useRef<THREE.Mesh>(null);
 
+	const { speed } = useControls({
+		speed: { value: 2, min: 0, max: 10.0 },
+	});
+
 	useFrame(({ clock }) => {
 		if (meshRef.current) {
-			meshRef.current.rotation.z = clock.getElapsedTime();
+			meshRef.current.rotation.z = clock.getElapsedTime() * speed;
 		}
 	});
 
